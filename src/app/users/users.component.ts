14841c83a08865e6f3c765c1b39dfa864b9cc5d0
Peter Gurský <peter.gursky@upjs.sk>
2023-02-21 15:16:47 +0100

    users from server

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,15 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/entities/user';
+import { UsersService } from 'src/services/users.service';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.css']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+
   users: User[] = [
     new User('Alica','alica@alica.sk'),
     new User('Bob', 'bobik@kubik.sk', 1, new Date(),'tajne'),
     {name:'Cyril', email:'cylinder@post.sk', password:'cyriloveHeslo',
      toString: () => 'name: Cyril' }];
+
+  activeUser?: User;
+
+  constructor(private usersService: UsersService){}
+
+  ngOnInit(): void {
+    this.usersService.getUsers().subscribe(users => this.users = users);
+  }
+
+  onUserClick(user: User) {
+    this.activeUser = user;
+  }
 }
