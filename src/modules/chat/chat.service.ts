a3d476ea81f878c5103efbd597d9c33542eae094
Peter Gurský <peter.gursky@upjs.sk>
2023-03-29 10:21:58 +0200

    chat

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -11,6 +11,7 @@ export class ChatService {
   url = environment.websocket_url;
   socket?: WebSocket;
   stompClient?: Client;
+  nick = '';
 
   constructor() { }
 
@@ -20,12 +21,45 @@ export class ChatService {
       this.stompClient = over(this.socket);
       this.stompClient.connect({},frame => subscriber.next(true), 
                                   error => subscriber.next(false));
+      return () => { // spusti sa ked nastane unsubscribe
+        this.disconnect();
+      }
     });
+  }
+
+  disconnect() {
+    this.stompClient?.disconnect(()=>{});
   }
 
   sendNickName(nick: string) {
 //    this.stompClient?.send('/app/hello', {}, JSON.stringify({name: nick}));
 //    this.stompClient?.send('/app/hello', {}, `{"name": "${nick}"}`);
     this.stompClient?.send('/app/hello', {}, '{"name": "' + nick + '"}');
+    this.nick = nick;
   }
+
+  sendMessage(message: string) {
+    this.stompClient?.send('/app/message', {}, JSON.stringify({name: this.nick, message}));
+  }
+
+  listenGreetings(): Observable<string> {
+    return new Observable(subscriber => {
+      this.stompClient?.subscribe('/topic/greetings', 
+        resp =>subscriber.next(JSON.parse(resp.body).content)
+      );
+    });
+  }
+
+  listenMessages(): Observable<ChatMessage> {
+    return new Observable(subscriber => {
+      this.stompClient?.subscribe('/topic/messages', 
+        resp =>subscriber.next(JSON.parse(resp.body))
+      );
+    });
+  }
+}
+
+export interface ChatMessage {
+  name: string;
+  message: string;
 }
--- src/modules/chat/chat/chat.component.ts
+++ src/modules/chat/chat/chat.component.ts
@@ -1,24 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { MessageService } from 'src/services/message.service';
-import { ChatService } from '../chat.service';
+import { ChatMessage, ChatService } from '../chat.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
   styleUrls: ['./chat.component.css']
 })
-export class ChatComponent {
+export class ChatComponent implements OnDestroy {
   nick = '';
+  messages: ChatMessage[] = [];
+  messageToSend = '';
+  connected = false;
+  connectSubscription?: Subscription;
+  @ViewChild('messageInput') messageInput? : ElementRef;
 
   constructor(private chatService: ChatService, private messageService: MessageService){}
 
+  ngOnDestroy(): void {
+    this.chatService.disconnect();
+  }
+  
   onConnect() {
-    this.chatService.connect().subscribe(isConnected => {
-      if(isConnected) {
-        this.chatService.sendNickName(this.nick);
-      } else {
-        this.messageService.errorMessage("Server not available");
-      }
-    });
+    this.connectSubscription = 
+      this.chatService.connect().subscribe(isConnected => {
+        if(isConnected) {
+          this.connected = true;
+          this.chatService.listenGreetings().subscribe(greeting =>{
+            this.messages = [...this.messages, {name: 'SERVER', message: greeting}];
+          });
+          this.chatService.listenMessages().subscribe(msg =>{
+            this.messages = [...this.messages, msg];
+          });
+          this.chatService.sendNickName(this.nick);
+          setTimeout(()=>this.messageInput?.nativeElement.focus(),0);
+        } else {
+          this.messageService.errorMessage("Server not available");
+        }
+      });
+  }
+
+  onSend() {
+    this.chatService.sendMessage(this.messageToSend);
+    this.messageToSend = '';
+    this.messageInput?.nativeElement.focus();
+  }
+
+  onDisconnect() {
+    this.connectSubscription?.unsubscribe();
+    this.connected = false;
   }
 }
